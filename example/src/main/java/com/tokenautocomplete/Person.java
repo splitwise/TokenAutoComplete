a3b64a73a7e3500cecce87dc532af0e4ff1a3a2b
wdullaer <wouter.dullaert@gmail.com>
2015-01-07 12:19:00 +0100

    Android Studio failed to add these files to the previous commit

---
+++ example/src/main/java/com/tokenautocomplete/Person.java
@@ -0,0 +1,25 @@
+package com.tokenautocomplete;
+
+import java.io.Serializable;
+
+/**
+ * Simple container object for contact data
+ *
+ * Created by mgod on 9/12/13.
+ * @author mgod
+ */
+public class Person implements Serializable{
+    private String name;
+    private String email;
+
+    public Person(String n, String e) {
+        name = n;
+        email = e;
+    }
+
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+
+    @Override
+    public String toString() { return name; }
+}

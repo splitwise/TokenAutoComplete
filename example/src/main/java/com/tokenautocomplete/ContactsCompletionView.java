900201863c585bb2f9e52e504dfd925002915d6f
Doyung Lee <doyunglee@Doyungs-MacBook-Pro.local>
2015-07-08 13:58:15 -0400

    remove unnecessary Log

--- example/src/main/java/com/tokenautocomplete/ContactsCompletionView.java
+++ example/src/main/java/com/tokenautocomplete/ContactsCompletionView.java
@@ -3,7 +3,6 @@ package com.tokenautocomplete;
 import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
